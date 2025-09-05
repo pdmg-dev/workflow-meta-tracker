9f4c0d6727c683d5693132d957eefff1ead344bf
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-09-05 22:51:23 +0800

    feat: update __all__ to include main blueprint

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -1,3 +1,6 @@
+# app/blueprints/__init__.py
+from .admin import admin_bp
 from .auth import auth_bp
+from .main import main_bp
 
-__all__ = ["auth_bp"]
+__all__ = ["admin_bp", "auth_bp", "main_bp"]
