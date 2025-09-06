fd3c538bd6e6143317a1f9cb3cf1652558795f11
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-09-06 12:53:08 +0800

    feat: seed new staff user for testing

--- scripts/seed.py
+++ scripts/seed.py
@@ -4,19 +4,32 @@ from app.extensions import db
 
 
 def seed_data():
+    # Admin User
     existing_identity = db.session.execute(
         db.select(AuthIdentity).filter_by(username="admin")
     ).scalar_one_or_none()
 
     if not existing_identity:
-        admin_user = User(
-            full_name="Administrator", role="admin", is_active=True
-        )
+        admin_user = User(full_name="Admin", role="admin", is_active=True)
         db.session.add(admin_user)
         db.session.flush()
 
         admin_identity = AuthIdentity(username="admin", user_id=admin_user.id)
         admin_identity.set_password("admin")
         db.session.add(admin_identity)
+
+    # Staff User
+    existing_identity = db.session.execute(
+        db.select(AuthIdentity).filter_by(username="staff")
+    ).scalar_one_or_none()
+
+    if not existing_identity:
+        staff_user = User(full_name="Staff", role="staff", is_active=True)
+        db.session.add(staff_user)
+        db.session.flush()
+
+        staff_identity = AuthIdentity(username="staff", user_id=staff_user.id)
+        staff_identity.set_password("staff")
+        db.session.add(staff_identity)
 
     db.session.commit()
