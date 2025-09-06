79c740c581db81dd96f8c429da229c8742642308
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-09-06 12:21:07 +0800

    refactor: implement authentication model to isolate auth and user profile and update login logic

--- app/blueprints/auth/__init__.py
+++ app/blueprints/auth/__init__.py
@@ -1,6 +1,15 @@
 # app/blueprints/auth/__init__.py
 from flask import Blueprint
 
+from app.blueprints.auth import models
+from app.extensions import db, login_manager
+
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
+
 from . import forms, views  # noqa: F401 E402
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(models.AuthIdentity, int(user_id))
--- app/blueprints/auth/views.py
+++ app/blueprints/auth/views.py
@@ -1,37 +1,41 @@
 # app/blueprints/auth/views.py
-
 from flask import flash, redirect, render_template, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
-from app.blueprints.users import models
 from app.extensions import db
 from app.utils.routing import redirect_to_dashboard
 
 from . import auth_bp
 from .forms import LoginForm
+from .models import AuthIdentity
 
 
 @auth_bp.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect_to_dashboard(current_user)
+
     form = LoginForm()
+
     if form.validate_on_submit():
-        user = db.session.execute(db.select(models.User).filter_by(username=form.username.data)).scalar_one_or_none()
-        if not user:
-            flash("Username not found. Please try again.")
-        elif not user.check_password(form.password.data):
-            flash("Incorrect password. Please try again.")
+        identity = (
+            db.session.query(AuthIdentity)
+            .filter_by(username=form.username.data)
+            .first()
+        )
+        if identity and identity.check_password(form.password.data):
+            login_user(identity)
+            flash("Logged in successfully.", "success")
+            return redirect_to_dashboard(identity.user)
         else:
-            login_user(user)
-            flash("Logged in successfully.")
-            return redirect_to_dashboard(user)
+            flash("Invalid username or password.", "error")
+
     return render_template("auth/login.html", form=form)
 
 
-@auth_bp.route("logout", methods=["POST"])
+@auth_bp.post("/logout")
 @login_required
 def logout():
     logout_user()
-    flash("Logged out.")
+    flash("Logged out.", "info")
     return redirect(url_for("auth.login"))
