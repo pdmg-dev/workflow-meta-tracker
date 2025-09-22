fdc89bb47b8f199d0067c73350b238c1e63ea0fa
Philip Daniel Gonzales <philipdanielgonzales@gmail.com>
2025-09-22 21:35:59 +0800

    fix: make sure to redirect use to dashboard page if not permitted by access control

--- app/utils/access_control.py
+++ app/utils/access_control.py
@@ -14,8 +14,7 @@ def require_roles(*allowed_roles):
             user_role_codes = {role.code for role in current_user.roles}
             if not any(role in user_role_codes for role in allowed_roles):
                 flash("You don't have permission to perform this action.", "danger")
-                return redirect(request.referrer or url_for("dashboard.dashboard"))
-
+                return redirect(request.referrer or url_for("tracker.dashboard"))
             return f(*args, **kwargs)
 
         return wrapped
